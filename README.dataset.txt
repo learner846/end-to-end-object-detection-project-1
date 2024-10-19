04adae419f717d67d2c5c826eab5eb1b23fc0b04
Suraj <sikarwaraman987@gmail.com>
2024-10-19 13:51:32 +0530

    Some changes

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# object detection > 2024-10-14 9:23am
+https://universe.roboflow.com/object-detection-bvxsa/object-detection-smge4
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- signlanguage/entity/config_entity.py
+++ signlanguage/entity/config_entity.py
@@ -30,6 +30,7 @@ class DataIngestionConfig:
 
 
 
+#<<<<<<< HEAD
 # @dataclass
 # class DataValidationConfig:
 #     data_validation_dir: str = os.path.join(
@@ -53,6 +54,31 @@ class DataIngestionConfig:
 #     no_epochs = MODEL_TRAINER_NO_EPOCHS
 
 #     batch_size = MODEL_TRAINER_BATCH_SIZE
+#=======
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_VALIDATION_DIR_NAME
+    )
+
+    valid_status_file_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_STATUS_FILE)
+
+    required_file_list = DATA_VALIDATION_ALL_REQUIRED_FILES
+
+
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME
+    )
+
+    weight_name = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME
+
+    no_epochs = MODEL_TRAINER_NO_EPOCHS
+
+    batch_size = MODEL_TRAINER_BATCH_SIZE
+#>>>>>>> 4f5404e (Some changes)
 
 
 
